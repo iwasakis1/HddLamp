9918bade7f9fa69a437afab5490223ab53ea30b7
chu arai <iwasakis1@outlook.jp>
2024-12-16 10:00:02 +0900

    プロジェクト ファイルを追加します。

---
+++ Form1.Designer.vb
@@ -0,0 +1,77 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'フォームがコンポーネントの一覧をクリーンアップするために dispose をオーバーライドします。
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Windows フォーム デザイナーで必要です。
+    Private components As System.ComponentModel.IContainer
+
+    'メモ: 以下のプロシージャは Windows フォーム デザイナーで必要です。
+    'Windows フォーム デザイナーを使用して変更できます。  
+    'コード エディターを使って変更しないでください。
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.components = New System.ComponentModel.Container()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.Timer1 = New System.Windows.Forms.Timer(Me.components)
+        Me.Label2 = New System.Windows.Forms.Label()
+        Me.NotifyIcon1 = New System.Windows.Forms.NotifyIcon(Me.components)
+        Me.SuspendLayout()
+        '
+        'Label1
+        '
+        Me.Label1.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle
+        Me.Label1.Location = New System.Drawing.Point(49, 41)
+        Me.Label1.Margin = New System.Windows.Forms.Padding(4, 0, 4, 0)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(53, 17)
+        Me.Label1.TabIndex = 0
+        '
+        'Timer1
+        '
+        Me.Timer1.Enabled = True
+        '
+        'Label2
+        '
+        Me.Label2.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle
+        Me.Label2.Location = New System.Drawing.Point(49, 59)
+        Me.Label2.Margin = New System.Windows.Forms.Padding(4, 0, 4, 0)
+        Me.Label2.Name = "Label2"
+        Me.Label2.Size = New System.Drawing.Size(53, 17)
+        Me.Label2.TabIndex = 1
+        '
+        'NotifyIcon1
+        '
+        Me.NotifyIcon1.Text = "HddLamp"
+        Me.NotifyIcon1.Visible = True
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(8.0!, 15.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(233, 141)
+        Me.Controls.Add(Me.Label2)
+        Me.Controls.Add(Me.Label1)
+        Me.Margin = New System.Windows.Forms.Padding(4, 4, 4, 4)
+        Me.Name = "Form1"
+        Me.Text = "Form1"
+        Me.ResumeLayout(False)
+
+    End Sub
+
+    Friend WithEvents Label1 As Label
+    Friend WithEvents Timer1 As Timer
+    Friend WithEvents Label2 As Label
+    Public WithEvents NotifyIcon1 As NotifyIcon
+End Class
---
+++ Form1.vb
@@ -0,0 +1,83 @@
+﻿Imports System.Windows.Forms.VisualStyles.VisualStyleElement
+
+Imports System.Security.Permissions
+
+'フォームのCreateParamsプロパティをオーバーライドする
+Public Class Form1
+    Private Disc1, Disc2 As New PerformanceCounter(),
+        icos(3) As Icon
+
+    Protected Overrides ReadOnly Property CreateParams() As CreateParams
+        <SecurityPermission(SecurityAction.Demand,
+        Flags:=SecurityPermissionFlag.UnmanagedCode)>
+        Get
+            Const WS_EX_TOOLWINDOW As Int32 = &H80
+            Const WS_POPUP As Int32 = &H80000000
+            Const WS_VISIBLE As Int32 = &H10000000
+            Const WS_SYSMENU As Int32 = &H80000
+            Const WS_MAXIMIZEBOX As Int32 = &H10000
+
+            Dim cp As System.Windows.Forms.CreateParams
+            cp = MyBase.CreateParams
+            cp.ExStyle = WS_EX_TOOLWINDOW
+            cp.Style = WS_POPUP Or WS_VISIBLE Or
+            WS_SYSMENU Or WS_MAXIMIZEBOX
+            cp.Height = 0
+            cp.Width = 0
+            Return cp
+        End Get
+    End Property
+
+    Private Sub Form1_Load(sender As Object, e As EventArgs) Handles Me.Load
+        '現在のコードを実行しているAssemblyを取得
+        Dim myAssembly As System.Reflection.Assembly =
+            System.Reflection.Assembly.GetExecutingAssembly()
+        '指定されたマニフェストリソースを読み込む
+        Dim myIcon As New Icon(myAssembly.GetManifestResourceStream("HddLamp.lamp_00.ico"))
+        NotifyIcon1.Icon = myIcon
+        NotifyIcon1.BalloonTipText = "HddLamp"
+        icos(0) = myIcon
+        myIcon = New Icon(myAssembly.GetManifestResourceStream("HddLamp.lamp_01.ico"))
+        icos(1) = myIcon
+        myIcon = New Icon(myAssembly.GetManifestResourceStream("HddLamp.lamp_02.ico"))
+        icos(2) = myIcon
+        myIcon = New Icon(myAssembly.GetManifestResourceStream("HddLamp.lamp_03.ico"))
+        icos(3) = myIcon
+    End Sub
+    Private Sub Timer1_Tick(sender As Object, e As EventArgs) Handles Timer1.Tick
+        Dim p1, p2 As Single
+        'Disk1
+        Disc1.CategoryName = "LogicalDisk"
+        'Disc1.CategoryName = "PhysicalDisk"
+        'Disc1.CounterName = "Disk Transfers/sec"
+        Disc1.CounterName = "Disk Reads/sec"
+        Disc1.InstanceName = "C:"
+
+        p1 = Disc1.NextValue()
+        'Label1.Text = p1.ToString()
+        Dim ipos = 0
+        If p1 > .0 Then
+            Label1.BackColor = Color.LightGreen
+            ipos += 1
+        Else
+            Label1.BackColor = Nothing
+        End If
+
+        Disc2.CategoryName = "LogicalDisk"
+        Disc2.CounterName = "Disk Writes/sec"
+        Disc2.InstanceName = "C:"
+        p2 = Disc2.NextValue()
+        'Label2.Text = p2.ToString()
+        If p2 > .0 Then
+            Label2.BackColor = Color.Red
+            ipos += 2
+        Else
+            Label2.BackColor = Nothing
+        End If
+        NotifyIcon1.Icon = icos(ipos)
+
+        'Debug.WriteLine(p1)
+    End Sub
+
+End Class
+
